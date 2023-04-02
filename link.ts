b59c7d692581e47591605c307683d999c585790e
Abidkk <121240127+Abidkk@users.noreply.github.com>
2023-04-02 18:04:24 +0500

    link in terminal
    link in terminal

---
+++ link.ts
@@ -0,0 +1,11 @@
+console.log('Hello This is link in terminal');
+import terminalLink from 'terminal-link';
+
+const link = terminalLink('My Website', 'https://abidkk.online');
+console.log(link);
+
+
+
+/*
+Run this command in terminal ==>>       npm install terminal-link
+*/
